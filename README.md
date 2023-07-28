9e3a7df758113654ede24c67e3d0f0d4979e9ec5
Carlos David Barreiros Reyes <davizi96@hotmail.com>
2023-07-28 00:04:59 -0300

    feat(setup): setup inicial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # reactjs-reddit
+
 Aplicação feita com React, TypeScript, Vite, RTK (Redux Toolkit) e tailwind, que consiste em ler dados da api de Reddit tendo as informações dos posts `hot`, `new` e `rising` do subreddit ReactJS.
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "add-icon": "url('/src/assets/images/add-icon.svg')",
+      },
+      colors: {
+        primary: "#6324C6",
+        "feedback-warning": "#FFB800",
+        "gray-dark-2": "#4C5667",
+        "gray-gray": "#A7B0BE",
+      },
+      fontFamily: {
+        sans: ["Mulish", ...defaultTheme.fontFamily.sans],
+      },
+      lineHeight: {
+        normal: "normal",
+      },
+    },
+  },
+  plugins: [],
+};
